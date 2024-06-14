9b6f2e692e3f560ebe93a34210c21a1e9b253ae4
emmanuel758 <emmanuelleuna758@gmail.com>
2024-06-14 09:24:29 +0100

    done with correction

--- e-learning/frontend/src/app/app-routing.module.ts
+++ e-learning/frontend/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/pages/home/home.component';
 import { InscriptionComponent } from './Components/pages/inscription/inscription.component';
 import { ConnexionComponent } from './Components/pages/connexion/connexion.component';
+import { HomeUserComponent } from './Components/pages/home-user/home-user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, title: "Institute - Accueil" },
   { path: 'register', component: InscriptionComponent, title: "Institute - Inscription" },
   { path: 'login', component: ConnexionComponent, title: "Institute - Connexion" },
-  { path: 'institute/user/home', component: InscriptionComponent, title: "Utilisateur - Tableau de bord" },
+  { path: '/user/home', component: HomeUserComponent, title: "Utilisateur - Tableau de bord" },
 
 ];
 
